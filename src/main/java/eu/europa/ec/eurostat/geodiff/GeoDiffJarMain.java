d276f9d0d58f5852d452b57862dfe3f6ff709184
jgaffuri <jgaf@protonmail.com>
2020-03-05 13:42:16 +0100

    skeleton

---
+++ src/main/java/eu/europa/ec/eurostat/geodiff/GeoDiffJarMain.java
@@ -0,0 +1,16 @@
+package eu.europa.ec.eurostat.geodiff;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class GeoDiffJarMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("start");
+	}
+
+}
