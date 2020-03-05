e6376973d87867ca14903344284f764aa93dfd98
jgaffuri <jgaf@protonmail.com>
2020-03-05 15:18:31 +0100

    test

---
+++ src/test/java/eu/europa/ec/eurostat/geodiff/GeoDiffJarMainTest.java
@@ -0,0 +1,20 @@
+package eu.europa.ec.eurostat.geodiff;
+
+import junit.framework.TestCase;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class GeoDiffJarMainTest extends TestCase {
+
+	/*
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(GeoDiffJarMainTest.class);
+	}*/
+
+	public void test() throws Exception {
+		//GeoDiffJarMain.main(new String[] {"-o", "target/test/out.gpkg"});
+	}
+
+}
