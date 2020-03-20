828ee4772d9276a9538a07f75178761a594d9c0f
jgaffuri <jgaf@protonmail.com>
2020-03-20 11:47:12 +0100

    new tests

--- src/test/java/eu/europa/ec/eurostat/geodiff/GeoDiffJarMainTest.java
+++ src/test/java/eu/europa/ec/eurostat/geodiff/GeoDiffJarMainTest.java
@@ -8,26 +8,25 @@ import junit.framework.TestCase;
  */
 public class GeoDiffJarMainTest extends TestCase {
 
-
 	public static void main(String[] args) {
 		junit.textui.TestRunner.run(GeoDiffJarMainTest.class);
 	}
 
 	public void test() throws Exception {
 		for(String gt : new String[] {"surf","lin","pt"})
-			GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_"+gt+"/"});
+			GeoDiffJarMain.main(new String[] {"-v1", "src/test/resources/ini_"+gt+".gpkg", "-v2", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_"+gt+"/"});
 	}
 
 	public void testRes() throws Exception {
 		for(String gt : new String[] {"surf"})
-			GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_res_"+gt+"/", "-res", "50"});
+			GeoDiffJarMain.main(new String[] {"-v1", "src/test/resources/ini_"+gt+".gpkg", "-v2", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_res_"+gt+"/", "-res", "50"});
 	}
 
 	public void testOutFormat() throws Exception {
 		for(String gt : new String[] {"surf","lin","pt"}) {
-			GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "gpkg"});
-			GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "shp"});
-			GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "geojson"});
+			GeoDiffJarMain.main(new String[] {"-v1", "src/test/resources/ini_"+gt+".gpkg", "-v2", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "gpkg"});
+			GeoDiffJarMain.main(new String[] {"-v1", "src/test/resources/ini_"+gt+".gpkg", "-v2", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "shp"});
+			GeoDiffJarMain.main(new String[] {"-v1", "src/test/resources/ini_"+gt+".gpkg", "-v2", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_of_"+gt+"/", "-of", "geojson"});
 		}
 	}
 }
